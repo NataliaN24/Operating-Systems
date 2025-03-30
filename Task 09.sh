439422848b411acebf5f09f594404cc79442ca19
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-03-30 16:29:31 +0300

    Delete Task 09.sh

--- Task 09.sh
+++
@@ -1,4 +0,0 @@
-#Копирайте /etc/services в home директорията си. Прочетете го с командата cat. (Ако този файл го няма, прочетете с cat произволен текстов файл напр. /etc/passwd)
-
-cp /etc/services ~/services
-cat ~/services
