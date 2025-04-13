1fc2d639e395c944a8c36b370413136851a1e346
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-04-13 14:58:53 +0300

    Update 05-b-2000.sh

--- Scripts/05-b-2000.sh
+++ Scripts/05-b-2000.sh
@@ -1,5 +1,12 @@
-2 #!/bin/bash
+#Да се напише shell скрипт, който приканва потребителя да въведе низ (име) и изпечатва "Hello, низ".
+
+#First solution
+  2 #!/bin/bash
   3
   4 echo -n "Enter your name:"
   5 read name
   6 echo "Hello $name"
+
+#Second solution
+read -p "Enter your name: " name
+echo "Hello, ${name}"
