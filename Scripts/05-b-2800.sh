d38b782fb4a68b98c41aba436b031d59d373c879
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-04-13 15:18:17 +0300

    Create 05-b-2800.sh

---
+++ Scripts/05-b-2800.sh
@@ -0,0 +1,16 @@
+#Да се напише shell скрипт, който приема точно един параметър и проверява дали подаденият му параметър
+#се състои само от букви и цифри.
+
+  1 #!/bin/bash
+  2
+  3 if [[ $# -ne 1 ]]; then
+  4 echo "It must contain only one parameter"
+  5 exit 1
+  6 fi
+  7
+  8 param="$1"
+  9 if [[ "$param" =~ ^[a-zA-Z0-9]+$ ]]; then
+ 10 echo "valid input"
+ 11 else
+ 12 echo "invalid input"
+ 13 fi
