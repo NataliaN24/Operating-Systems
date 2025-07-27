8513b9a89be6cd1aab967352ecd50c0d9fc47dd1
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-07-28 00:19:47 +0300

    Update 02-a-6000.sh

--- Commands/02-a-6000.sh
+++ Commands/02-a-6000.sh
@@ -1,5 +1,5 @@
 #Намерете файловете в /bin, които могат да се четат, пишат и изпълняват от всички.
-find /bin -perm 777
+find /bin -perm -777
 
 #second solution
  find /bin -perm -u=rwx,g=rwx,o=rwx
