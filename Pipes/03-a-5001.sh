2b0583c5a8713e1f96048bf101a62846640cdcea
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-07-30 00:41:41 +0300

    Update 03-a-5001.sh

--- Pipes/03-a-5001.sh
+++ Pipes/03-a-5001.sh
@@ -2,7 +2,7 @@
 #(hint: 'man 5 passwd' за информация какъв е форматът на /etc/passwd)
 
 #Solution 1
-grep -v "/bin/bash" /etc/passwd | wc -l
+ cat /etc/passwd | grep -v "/bin/bash" | wc -l
 
 #Solution 2
 cat /etc/passwd | cut -d ':' -f 7 | grep -v '/bin/bash' | wc -l
