10ea4cf9aa58a5318499f9ee85276da9696baf91
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-08-02 21:26:01 +0300

    Create 03-b-8600.sh

---
+++ Pipes/03-b-8600.sh
@@ -0,0 +1,17 @@
+# Shell Script-овете са файлове, които по конвенция имат разширение .sh. Всеки
+# такъв файл започва с "#!<interpreter>" , където <interpreter> указва на
+# операционната система какъв интерпретатор да пусне (пр: "#!/bin/bash",
+# "#!/usr/bin/python3 -u").
+
+# Намерете всички .sh файлове в директорията `/usr` и нейните поддиректории, и
+# проверете кой е най-често използваният интерпретатор.
+
+#not the best option
+find /usr -type f -name  '*.sh' -exec head -n 1 {} \; | grep '^#!' | sed 's/^#!//' |sort | uniq -c | sort -nr
+
+#better options
+find /usr/ -iname "*.sh" | xargs -I{} head -n 1 {} | grep "^#!" | tr -d " " | sort | uniq -c | sort -nr | head -n 1 | cut -d '!' -f2
+
+#or
+
+find /usr/ -iname '*.sh' -exec head -n 1 {} \; | grep "^#!" | cut -d "!" -f 2 | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
