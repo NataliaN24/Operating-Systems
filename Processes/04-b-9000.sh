a729f5e1925b76cae1c61a48a6429202045d2934
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-08-04 00:33:58 +0300

    Create 04-b-9000.sh

---
+++ Processes/04-b-9000.sh
@@ -0,0 +1,3 @@
+#Да се отпечатат PID на всички процеси, които имат повече деца от родителския си процес.
+
+ps -eo pid=,ppid= | awk '{childCount[$2]++; parent[$1]=$2; } END { for (pid in parent) {ppid = parent[pid]; if(childCount[pid] > childCount[ppid]) print pid; } }'
