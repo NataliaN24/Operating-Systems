92dd4cba1174453f9f2f915dd6237f59dd4e395c
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-08-17 19:21:33 +0300

    Update 2016-SE-01.sh

--- Exams/Shell/Scripts/2016-SE-01.sh
+++ Exams/Shell/Scripts/2016-SE-01.sh
@@ -1,1 +1,19 @@
+#Напишете shell скрипт, който по подаден един позиционен параметър, ако този
+#параметър е директория, намира всички symlink-ове в нея и под-директориите `и с несъществуващ
+#destination.
+
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+  exit 1;
+fi
+
+$1=dir
+
+if [[ ! -d "$dir" ]]; then
+  echo "Not a directory"
+  exit 2
+fi
+
+find "$dir" -type l ! -exec test -e {} \; -print
 
