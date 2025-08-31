51f31b835473694bb2e774c4df6eb0af548772d6
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-08-31 20:21:24 +0300

    Delete Processes directory

--- Processes/04-b-9000.sh
+++
@@ -1,7 +0,0 @@
-#Да се отпечатат PID на всички процеси, които имат повече деца от родителския си процес.
-
-$ps -eo pid=,ppid= | awk '{childCount[$2]++; parent[$1]=$2; } END { for (pid in parent) {ppid = parent[pid]; if(childCount[pid] > childCount[ppid]) print pid; } }'
-
-#or
-
-ps -A -o pid,ppid | awk '{parent[$1]=$2;children[$2]++;} END{for (child in children) {if (children[child] > children[parent[child]]) print child}}'
