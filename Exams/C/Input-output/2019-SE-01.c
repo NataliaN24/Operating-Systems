de21319fcd7aa13ee129bebb9d9f653ea0e0463b
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-08-31 19:57:03 +0300

    Update 2019-SE-01.c

--- Exams/C/Input-output/2019-SE-01.c
+++ Exams/C/Input-output/2019-SE-01.c
@@ -1,108 +1,137 @@
+ В приложната статистика често се използват следните описателни статистики за дадена
+извадка:
+47
+• средна стойност ̄𝑥 = 1
+𝑁
+𝑁
+∑
+𝑖=1
+𝑥𝑖
+• дисперсия 𝐷 = 1
+𝑁
+𝑁
+∑
+𝑖=1
+(𝑥𝑖 − ̄𝑥)2
+където {𝑥1
+, 𝑥2
+, … , 𝑥𝑁} са стойностите на извадката, а 𝑁 е техния брой.
+Напишете програма на C, която приема задължителен параметър – име на двоичен файл. Файлът
+съдържа информация за потребители, които са влизали в системата, и се състои от наредени петорки
+от следните елементи и техните типове:
+• уникален потребителски идентификатор (UID) uint32_t
+• запазено1 uint16_t – не се използва в задачата
+• запазено2 uint16_t – не се използва в задачата
+• време1 uint32_t – момент на започване на сесията (Unix time)
+• време2 uint32_t – момент на завършване на сесията (Unix time)
+За потребители, които са имали сесии, квадратът на продължителността на които е по-голям от дисперсията 𝐷 на продължителността на всички сесии във файла, програмата трябва да изведе на STDOUT
+потребителският им идентификатор и продължителността на най-дългата им сесия.
+
+    #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <err.h>
-#include <stdint.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <stdio.h>
+#include <math.h>
 
-const int MAX_SIZE = 16384;
-const int MAX_USERS = 2048;
-
-typedef struct __attribute__((packed)) {
+typedef struct {
     uint32_t uid;
-    uint16_t _;
-    uint16_t __;
-    uint32_t start_session;
-    uint32_t end_session;
-} user_data;
+    uint16_t reserved1;
+    uint16_t reserved2;
+    uint32_t time1;
+    uint32_t time2;
+} record_t;
 
-typedef struct __attribute__((packed)) {
-    uint32_t uid;
-    uint32_t session_duration;
-} user_session;
-
-void close_safe(int fd);
-double calculate_dispersion(user_session* sessions, uint32_t nums_count, double average);
-
-double calculate_dispersion(user_session* sessions, uint32_t nums_count, double average) {
-    double sum = 0;
-    for (uint32_t i = 0; i < nums_count; i++) {
-        double diff = sessions[i].session_duration > average ? sessions[i].session_duration - average : average - sessions[i].session_duration;
-        sum += (diff * diff);
-    }
-
-    return (sum / nums_count);
-}
-
-void close_safe(int fd) {
-    int errno_ = errno;
-    close(fd);
-    errno = errno_;
-}
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     if (argc != 2) {
-        errx(1, "Invalid arguments. Usage: %s <file_name>", argv[0]);
+        errx(1, "Usage: %s <file>", argv[0]);
     }
 
-    const char* file_name = argv[0];
+    const char *file_name = argv[1];
+    int fd = open(file_name, O_RDONLY);
+    if (fd < 0) err(2, "Cannot open %s", file_name);
 
-    struct stat st;
-    if (stat(file_name, &st) == -1) {
-        err(4, "Could not stat file %s", file_name);
+    // Четем целия файл в памет (максимум 100 000 записа)
+    off_t file_size = lseek(fd, 0, SEEK_END);
+    if (file_size < 0) err(3, "lseek failed");
+    if (lseek(fd, 0, SEEK_SET) < 0) err(4, "lseek failed");
+
+    size_t n = file_size / sizeof(record_t);
+    if (n == 0) {
+        close(fd);
+        return 0;
     }
 
-    if (st.st_size % sizeof(user_data) != 0) {
-        errx(5, "Invalid file content");
+    record_t *records = malloc(n * sizeof(record_t));
+    if (!records) errx(5, "Memory allocation failed");
+
+    ssize_t bytes_read = read(fd, records, n * sizeof(record_t));
+    if (bytes_read < 0) err(6, "Read error");
+    if ((size_t)bytes_read != n * sizeof(record_t)) errx(7, "Could not read full file");
+
+    close(fd);
+
+    // Изчисляваме продължителностите
+    double sum = 0.0;
+    double sum_sq = 0.0;
+    uint32_t *durations = malloc(n * sizeof(uint32_t));
+    if (!durations) errx(8, "Memory allocation failed");
+
+    for (size_t i = 0; i < n; i++) {
+        uint32_t dur = records[i].time2 - records[i].time1;
+        durations[i] = dur;
+        sum += dur;
     }
 
-    user_session sessions[MAX_SIZE];
-    user_data ud;
-    int fd;
-    if ((fd = open(file_name, O_RDONLY)) == -1) {
-        err(2, "Could not open file %s", file_name);
-    }
+    double mean = sum / n;
 
-    int bytes_count, counter = 0;
-    uint32_t duration, sum = 0;;
-    while((bytes_count = read(fd, &ud, sizeof(ud))) > 0) {
-        if (ud.end_session < ud.start_session) {
-            close_safe(fd);
-            errx(7, "Invalid session interval");
-        }
-        duration = ud.end_session - ud.start_session;
-        sum += duration;
-        sessions[counter] = (user_session){ud.uid, duration};
-        counter++;
+    double variance = 0.0;
+    for (size_t i = 0; i < n; i++) {
+        double diff = durations[i] - mean;
+        variance += diff * diff;
     }
+    variance /= n;
 
-    close_safe(fd);
-    if (bytes_count == -1) {
-        err(3, "Could not read from file %s", file_name);
-    }
+    // Намираме най-дългата сесия за всеки UID и проверяваме условието
+    typedef struct {
+        uint32_t uid;
+        uint32_t max_dur;
+    } user_t;
 
-    double average = sum / counter;
-    double dispersion = calculate_dispersion(sessions, counter, average);
-    uint32_t us[MAX_USERS];
-    for (int i = 0; i < MAX_USERS; i++) {
-        us[i] = 0;
-    }
+    user_t *users = malloc(n * sizeof(user_t));
+    if (!users) errx(9, "Memory allocation failed");
+    size_t user_count = 0;
 
-    for (int i = 0; i < counter; i++) {
-        uint32_t sq = (sessions[i].session_duration * sessions[i].session_duration);
-        if (sq > dispersion) {
-            if (us[sessions[i].uid] < sq) {
-                us[sessions[i].uid] = sq;
+    for (size_t i = 0; i < n; i++) {
+        uint32_t uid = records[i].uid;
+        uint32_t dur = durations[i];
+        int found = 0;
+        for (size_t j = 0; j < user_count; j++) {
+            if (users[j].uid == uid) {
+                if (dur > users[j].max_dur) users[j].max_dur = dur;
+                found = 1;
+                break;
             }
         }
-    }
-
-    for (uint32_t i = 0; i < MAX_USERS; i++) {
-        if (us[i] > 0) {
-            dprintf(1, "%d %d\n", i, us[i]);
+        if (!found) {
+            users[user_count].uid = uid;
+            users[user_count].max_dur = dur;
+            user_count++;
         }
     }
+
+    for (size_t i = 0; i < user_count; i++) {
+        if ((double)(users[i].max_dur) * (double)(users[i].max_dur) > variance) {
+            printf("%u %u\n", users[i].uid, users[i].max_dur);
+        }
+    }
+
+    free(records);
+    free(durations);
+    free(users);
+
+    return 0;
 }
+
+Можете да разчитате на това, че във файла ще има не повече о
