cd2c42a370942cd1290077748ab97ab5874adcc1
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-08-31 19:54:56 +0300

    Update 2018-SE-04.c

--- Exams/C/Input-output/2018-SE-04.c
+++ Exams/C/Input-output/2018-SE-04.c
@@ -1,107 +1,3 @@
-#include <stdint.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <err.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-int fds[2];
-
-int open_safe(const char* file_name, int flags, mode_t mode);
-void close_all(void);
-void load_arr(uint16_t* numbers, const char* file_name);
-int comp(const void* a, const void* b);
-void load_file(uint16_t* numbers, int numbers_count,  const char* file_name);
-
-int comp(const void* a, const void* b) {
-    const uint16_t* first = (const uint16_t*)a;
-    const uint16_t* second = (const uint16_t*)b;
-
-    if(*first < *second) {
-        return -1;
-    } else if (*first > *second) {
-        return 1;
-    }
-
-    return 0;
-}
-
-void load_arr(uint16_t* numbers, const char* file_name) {
-    uint16_t number;
-    int bytes_count, index = 0;
-    while((bytes_count = read(fds[0], &number, sizeof(number))) == sizeof(number)) {
-        numbers[index] = number;
-        index++;
-    }
-
-    if (bytes_count == -1) {
-        close_all();
-        err(4, "Could not read from %s", file_name);
-    }
-}
-
-void load_file(uint16_t* numbers, int numbers_count,  const char* file_name) {
-    for (int i = 0; i < numbers_count; i++) {
-        if (write(fds[1], &numbers[i], sizeof(numbers[i])) == -1) {
-            close_all();
-            err(5, "Could not write to %s", file_name);
-        }
-    }
-}
-
-void close_all(void) {
-    int errno_ = errno;
-    for (int i = 0; i < 2; i++) {
-        if (fds[i] >= 0) {
-            close(fds[i]);
-        }
-    }
-
-    errno = errno_;
-}
-
-int open_safe(const char* file_name, int flags, mode_t mode) {
-    int fd;
-    if ((fd = open(file_name, flags, mode)) == -1) {
-        close_all();
-        err(1, "Could not open file %s", file_name);
-    }
-
-    return fd;
-}
-
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        errx(1, "Invalid arguments. Usage %s <input_file> <output_file>", argv[0]);
-    }
-
-    const char *input_file = argv[1], *output_file = argv[2];
-
-    struct stat s1;
-
-    if(stat(input_file, &s1) == -1) {
-        err(2, "Could not stat %s", input_file);
-    }
-
-    if(s1.st_size % 2 != 0) {
-        errx(3, "File %s does not contain uint16_t numbers only", input_file);
-    }
-
-    fds[0] = open_safe(input_file, O_RDONLY, S_IRUSR | S_IWUSR);
-    fds[1] = open_safe(output_file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
-    int numbers_count = s1.st_size / sizeof(uint16_t);
-    uint16_t* numbers = malloc(numbers_count * sizeof(uint16_t));
-    load_arr(numbers, input_file);
-    qsort(numbers, numbers_count, sizeof(uint16_t), comp);
-    load_file(numbers, numbers_count, output_file);
-    close_all();
-    free(numbers);
-    exit(0);
-}
-
 //Напишете програма на C, която приема два параметъра – имена на файлове:
 //• примерно извикване: ./main input.bin output.bin
 //• файловете input.bin и output.bin се третират като двоични файлове, състоящи се от uint16_t
@@ -112,3 +8,73 @@ int main(int argc, char** argv) {
 //• endianness-ът на машината, създала файла input.bin е същият, като на текущата машина
 //• ограничения на ресурси: програмата трябва да работи с употреба на максимум 256 KB RAM
 //и 2 MB дисково пространство.
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <err.h>
+
+int compare_uint16(const void *a, const void *b) {
+    uint16_t x = *(uint16_t*)a;
+    uint16_t y = *(uint16_t*)b;
+    if (x < y) return -1;
+    if (x > y) return 1;
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        errx(1, "Usage: %s input.bin output.bin", argv[0]);
+    }
+
+    const char *input_name = argv[1];
+    const char *output_name = argv[2];
+
+    int fd_in = open(input_name, O_RDONLY);
+    if (fd_in < 0) err(2, "Cannot open %s", input_name);
+
+    int fd_out = open(output_name, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (fd_out < 0) err(3, "Cannot create %s", output_name);
+
+    // Разбираме размера на файла
+    off_t file_size = lseek(fd_in, 0, SEEK_END);
+    if (file_size < 0) err(4, "lseek failed");
+
+    if (file_size % sizeof(uint16_t) != 0) {
+        errx(5, "Invalid input file size");
+    }
+
+    size_t n = file_size / sizeof(uint16_t);
+    if (n > 65535) {
+        errx(6, "Too many numbers in input file");
+    }
+
+    // Връщаме указателя в началото
+    if (lseek(fd_in, 0, SEEK_SET) < 0) err(7, "lseek failed");
+
+    uint16_t *numbers = malloc(n * sizeof(uint16_t));
+    if (!numbers) errx(8, "Memory allocation failed");
+
+    ssize_t bytes_read = read(fd_in, numbers, n * sizeof(uint16_t));
+    if (bytes_read < 0) err(9, "Read error");
+    if ((size_t)bytes_read != n * sizeof(uint16_t)) {
+        errx(10, "Could not read full input");
+    }
+
+    close(fd_in);
+
+    qsort(numbers, n, sizeof(uint16_t), compare_uint16);
+
+    ssize_t bytes_written = write(fd_out, numbers, n * sizeof(uint16_t));
+    if (bytes_written < 0) err(11, "Write error");
+    if ((size_t)bytes_written != n * sizeof(uint16_t)) {
+        errx(12, "Could not write full output");
+    }
+
+    close(fd_out);
+    free(numbers);
+
+    return 0;
+}
