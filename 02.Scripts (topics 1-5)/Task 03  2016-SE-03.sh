00b7bd16da45e2af677acd7834e91c64cd1c8ded
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-02-21 14:35:51 +0200

    Create Task 03  2016-SE-03.sh

---
+++ 02.Scripts (topics 1-5)/Task 03  2016-SE-03.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+if [[ $# -ne 0 ]];then
+  echo "no need for arg""
+  exit 1
+fi
+
+if [[ "$(id -u)" -ne 0 ]];then
+  echo "must be root"
+  exit 2
+fi
+#we extract only the first and sicth field ,while the field separator is : we split these two columns
+#into user col uid col gid col  and home col and for each of them we check if the dir is "home"
+#if not we have found it 
+
+cut -d ":" -f1,3,4,6 /etc/passwd | while IFS=: read -r user uid gid home;do
+#do not have homedir
+  if [[ ! -d "$home" ]]; then
+    echo "$user has no homedir: $home "
+    continue
+  fi
+read -r h_uid h_gid perm < <(stat -c '%u %g %a' "$home" 2>/dev/null)
+if [[ -z "$perm" ]];then
+  echo "$user :cannot stat $home"
+  continue;
+fi
+#${variable:offset:length}
+owner=${perm:0:1}
+group=${perm:1:1}
+other=${perm:2:1}
+
+ #by default we accept that user is others bc if he is not owner and not in group linux will se in others
+  #1. Ако си owner → гледа owner
+  #2. Иначе ако си в group → гледа group
+  #3. Иначе → гледа others
+  
+ digit=$other
+ if [[ "$uid" -eq "$h_uid" ]];then
+   digit=$owner
+  elif [[ "$gid" -eq "$h_gid" ]];then
+    digit=$group
+  fi
+  
+if ! ((digit & 2)); then
+  echo "$user cannot write in $home"
+fi
+
+
+
+  #-r казва на read:Не интерпретирай backslash-и
