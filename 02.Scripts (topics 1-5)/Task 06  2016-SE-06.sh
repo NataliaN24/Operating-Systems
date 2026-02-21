c011ac572a79b56578d9ee50cd278e3f2e6490b1
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-02-21 16:22:54 +0200

    Create Task 06  2016-SE-06.sh

---
+++ 02.Scripts (topics 1-5)/Task 06  2016-SE-06.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+  echo "one argument please"
+  exit 1;
+fi
+
+if [[ ! -e $1 ]]; then
+  echo "file doesn't exist"
+  exit 2;
+fi
+
+cut -d ' ' -f4- "${1}" | sort > tempFile    #from 4 th col till the end
+i=0
+while read line
+do
+  echo "$((++i)). "$line"
+done < tempFile
+
+rm tempFile
