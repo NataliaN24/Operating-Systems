0f12cecbf8dfadd6df7b9437f4ec8a87f0bad5ec
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-02-22 17:26:05 +0200

    Create Task 18 2018-SE-03.sh

---
+++ 02.Scripts (topics 1-5)/Task 18 2018-SE-03.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+  exit 1
+fi
+
+
+if [[ ! -f "$1" || ! -f "$2"]];then
+  exit 1;
+fi 
+in="$1"
+out="$2"
+
+while IFS= read -r line
+do
+  id=$(echo "$line" | cut -d ',' -f1)
+  rest=$(echo "$line" | cut -d ',' -f2-)
+  found=0
+
+  while IFS= read -r existing
+  
+   old_id=$(echo "$existing" | cut -d ',' -f1)
+   old_rest=$(echo "$existing" | cut -d ',' -f2-)
+  do
+    if [[ "$rest" == "$old_rest" ]];then
+    found=1
+    if [[ "$id" -lt "$old_id" ]];then
+      grep -v "^$old_id,$old_rest$" "$out" >tmp
+      mv tmp "$out"
+
+      echo "$line" >> "$out"
+      fi
+    fi
+      
+  done <"$out"
+  if [[ $found -eq 0 ]]; then
+      echo "$line" >> "$out"
+  fi 
+
+done < "$in"
