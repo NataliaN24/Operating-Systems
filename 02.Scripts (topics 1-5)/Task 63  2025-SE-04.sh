fff322c0f99b8df396e453e0395ff28324dc76db
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-03-14 20:19:59 +0200

    Create Task 63  2025-SE-04.sh

---
+++ 02.Scripts (topics 1-5)/Task 63  2025-SE-04.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+  exit 1
+fi
+
+oldDir="$1"
+newDir="$2"
+
+if [[ ! -d "$oldDir" ]]; then
+  exit 2
+fi
+
+while read -r file; do
+  relFile=$(echo "$file" | sed "s#^$oldDir/##")
+  relDir=$(dirname "$relFile")
+  baseName=$(basename "$file" .bcf)
+
+  mkdir -p "$newDir/$relDir"
+  newFile="$newDir/$relDir/$baseName.bcf2"
+  : > "$newFile"
+
+  key=$(cut -d '=' -f1 "$file" | sort | uniq -c | sort -nr)
+
+  while read -r k; do
+      howManyTimes=$(echo "$k" | sed 's/^ *//' | cut -d ' ' -f1)
+      onlyKey=$(echo "$k" | sed 's/^ *//' | cut -d ' ' -f2)
+
+      value=$(grep "^$onlyKey=" "$file" | cut -d '=' -f2-)
+
+      if [[ "$howManyTimes" -eq 1 ]]; then
+          echo "$onlyKey: $value" >> "$newFile"
+      else
+          echo "$onlyKey:" >> "$newFile"
+          while read -r values; do
+              echo "- $values" >> "$newFile"
+          done <<< "$value"
+      fi
+
+  done <<< "$key"
+
+done < <(find "$oldDir" -type f -name "*.bcf")
