be4e1b2e1fad8d99e407ce99a6ecf9ecabf33f24
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-04-25 20:33:31 +0300

    Create Task 12 2023-IN-01.c

---
+++ 04.C (processes, pipes, input&output)/Task 12 2023-IN-01.c
@@ -0,0 +1,92 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/wait.h>
+#include <err.h>
+#include <errno.h>
+
+const char *words[]={ "tic ", "tac ", "toe\n"};
+
+int main(int argc, char* argv[])
+{
+   if(argc!=3)
+   {
+     err(1,"error");
+   }
+       char *endptr;
+
+
+       long NC=strtol(argv[1],&endptr,10); ///10 значи десетична бройна система.
+       if ( *end!='\0' || NC <1 ||NC>7)
+       {
+              errx(1, "invalid NC");
+       }
+       long WC=strtol(argv[2],&end,10);
+        if ( *end!='\0' || WC <1 ||WC>35)
+       {
+              errx(1, "invalid WC");
+       }
+       int total=NC+1; //child+parent
+       int p[8][2];//max NC=7 -> 7 childs+1 parent 
+       for(int i=0;i<total;i++)
+       {
+              if(pipe(p[1])<0)
+              {
+                     err(1,"error");
+              }
+       }
+       for(int i=1;i<total;i++)//bc i=0 is parent
+       {
+              pid_t pid=fork();
+              if(pid<0)
+              {
+                      err(1,"error");
+              }
+              if(pid==0)
+              {
+                     int id=i;
+                     int next=(id +1)%total;
+                     int k;
+              
+                     while(1)
+                     {
+                            if(read(p[id][0],&k,sizeof(k))!=sizeof(k))
+                            {
+                                   err(1,"error");
+                            }
+                            if(k>=WC)
+                            {
+                                   write(p[next][1],&k,sizeof(k));
+                                   exit(0);
+                            }
+                            write(1,words[k%3],4);
+                            k++;
+                            write(p[next][1],&k,sizeof(k));
+
+                            }
+              }
+       }
+       int k=0;
+       write(p[0][1],&k,sizeof(k));
+       while(1)
+       {
+              if(read(p[0][0],&k,sizeof(k))!=sizeof(k))
+              {
+                     err(1,"error");
+              }
+              if(k>=WC )
+              {
+                     write(p[1][1],&k,sizeof(k));
+                     break;
+              }
+              write (1,words[k%3],4);
+              k++;
+              write(p[1][1],&k,sizeof(k));
+
+       }
+       for(int i=0;i<NC;i++)
+       {
+              wait(NULL);
+       }
+
+}
