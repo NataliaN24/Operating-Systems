28c362a4d7c4ca037afa28f68fcc59f9fdc488db
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-01 19:25:12 +0300

    Update Task 10  2017-SE-01.sh

--- 02.Scripts (topics 1-5)/Task 10  2017-SE-01.sh
+++ 02.Scripts (topics 1-5)/Task 10  2017-SE-01.sh
@@ -11,19 +11,39 @@ destination (счупени symlinkове).
 #!/bin/bash
 
 if [[ $# -lt 1 || $# -gt 2 ]]; then
-  exit 1;
+    echo "Usage: $0 DIR [N]" >&2
+    exit 1
 fi
 
-dir="${1}";
-if [[ ! -d "$dir" ]];then
-  exit 2;
+dir="$1"
+
+if [[ ! -d "$dir" ]]; then
+    echo "First argument must be directory" >&2
+    exit 2
 fi
 
-if [[ $# -eq 2 ]];then
-  num="${2}"
-  find "$dir" -type f -links +"(($num-1))" //because -links + N means more than N and not at least N
+tmp=$(mktemp)
+
+if [[ $# -eq 2 ]]; then
+    n="$2"
+
+    find "$dir" -type f -printf '%n %p\n' > "$tmp"
+
+    while read -r links file; do
+        if [[ "$links" -ge "$n" ]]; then
+            echo "$file"
+        fi
+    done < "$tmp"
+
 else
-  find "$dir" -type l ! -exec test -e {} \; -print
+    find "$dir" -type l -printf '%p\n' > "$tmp"
+
+    while read -r link; do
+        if [[ ! -e "$link" ]]; then
+            echo "$link"
+        fi
+    done < "$tmp"
 fi
 
+rm -f "$tmp"
 
