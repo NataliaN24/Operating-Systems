338234cdeb8b33efbc142145175de89ce02715d6
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-06 11:50:28 +0300

    Update Task 23  2020-SE-02.sh

--- 02.Scripts (topics 1-5)/Task 23  2020-SE-02.sh
+++ 02.Scripts (topics 1-5)/Task 23  2020-SE-02.sh
@@ -1,45 +1,42 @@
 #!/bin/bash
 
 if [[ $# -ne 1 ]]; then
-  echo "Usage: $0 <logfile>" >&2
-  exit 1
+    echo "Usage: $0 logfile"
+    exit 1
 fi
 
-logfile="$1"
-if [[ ! -f "$logfile" || ! -r "$logfile" ]]; then
-  echo "Error: cannot read file: $logfile" >&2
-  exit 1
+file="$1"
+
+if [[ ! -f "$file" ]]; then
+    echo "File does not exist"
+    exit 1
 fi
 
-#35.223.122.181 dir.bg - [03/Apr/2020:17:25:06 -0500] GET / HTTP/1.1 302 0 "-" "Zend_Http_Client"
-#94.228.82.170 del.bg - [03/Apr/2020:17:25:06 -0500] POST /auth HTTP/2.0 400 153 "foo bar" "<UA>"
+tmp=$(mktemp)
 
-top_sites=$(
-  cut -d ' ' -f2 "$logfile" \
-    | sort \
-    | uniq -c \
-    | sort -nr \
-    | head -3 \
-    | awk '{print $2}'
-)
+# top 3 сайта с най-много заявки
+cut -d ' ' -f2 "$file" |
+sort |
+uniq -c |
+sort -nr |
+head -n 3 |
+awk '{print $2}' > "$tmp"
 
-for site in $top_Sites
-do
+while read -r site; do
 
- # всички заявки за този сайт
-allQueries=$(grep -E "^[^ ]+$site"  "$logfile")
+    http2=$(grep " $site " "$file" | grep "HTTP/2.0" | wc -l)
 
-# HTTP/2.0 заявки
-http2=$(echo "$allQueries" | grep -c "HTTP/2.0")
+    non_http2=$(grep " $site " "$file" | grep -v "HTTP/2.0" | wc -l)
 
-# всички заявки
-totalQueriesNumber=$(echo "$allQueries" | wc -l)
+    echo "$site HTTP/2.0: $http2 non-HTTP/2.0: $non_http2"
 
-nonHttp2=$((totalQueriesNumber-http2))
+    grep " $site " "$file" |
+    awk '$9 > 302 { print $1 }' |
+    sort |
+    uniq -c |
+    sort -nr |
+    head -n 5
 
-echo "$site HTTP/2.0: $http2 non-HTTP/2.0: $nonHttp2"
+done < "$tmp"
 
-clients=$(echo "$allQueries" | awk '$8 >302 {print $1}; | sort | uniq - | sort -nr | head -n 5)
-echo "$clients"
-
-done
+rm -f "$tmp"
