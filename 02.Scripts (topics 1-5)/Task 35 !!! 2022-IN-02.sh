ce2b5cef2638711b76d15260398a6d479f84ab32
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-09 20:50:33 +0300

    Update Task 35 !!! 2022-IN-02.sh

--- 02.Scripts (topics 1-5)/Task 35 !!! 2022-IN-02.sh
+++ 02.Scripts (topics 1-5)/Task 35 !!! 2022-IN-02.sh
@@ -1,68 +1,112 @@
 #!/bin/bash
 
+if [[ $# -ne 1 ]]; then
+    exit 1
+fi
+
+days="$1"
+
 user=$(whoami)
-if [[ $user != "oracle" && $user != "grid" ]];then
-  exit 1
-fi
-if [[ $# -ne 1]];then
-  exit 2;
-fi
-if [[ ! $1 =~ ^[0-9]+$ ]]; then
-  exit 2
+
+if [[ "$user" != "grid" && "$user" != "oracle" ]]; then
+    exit 2
 fi
 
-machine=$(hostname)
-
-if [[ -z $ORACLE_BASE ]];then 
-  exit 2;
-fi
-if [[ -z$ORACLE_HOME ]];then 
-  exit 2;
-fi
-if [[ -z $ORACLE_SID ]];then 
-  exit 2;
+if [[ -z "$ORACLE_BASE" || -z "$ORACLE_HOME" || -z "$ORACLE_SID" ]]; then
+    exit 3
 fi
 
-sqlplus=$(find $ORACLE_HOME/bin/sqlplus -executable)
-if [[ -z $sqlplus ]]; then
-  exit 4;
+sqlPlus="$ORACLE_HOME/bin/sqlplus"
+
+if [[ ! -x "$sqlPlus" ]]; then
+    exit 4
 fi
 
 role="SYSDBA"
-if [[ $user == "grid"]]; then
-  role="SYSASM"
-fi
-sqlplusOutput=$($sqlplus -SL "/ as $role " @foo.sql)
-exitStatus=$?
 
-diag_base="$ORACLE_HOME"
-if [[ $exitStatus -eq 0  ]];then
-  diag_base=$(echo $sqlplusOutput | tail -n +4 | head -n1)
+if [[ "$user" == "grid" ]]; then
+    role="SYSASM"
+fi
+
+result=$("$sqlPlus" -SL "/ as $role" @foo.sql)
+
+if [[ $? -ne 0 ]]; then
+    exit 5
+fi
+
+diagnostic_dest=$(echo "$result" | tail -n +4 | head -n 1 | tr -d ' ')
+
+diag_base="$ORACLE_BASE"
+
+if [[ -n "$diagnostic_dest" ]]; then
+    diag_base="$diagnostic_dest"
 fi
 
 diag_dir="$diag_base/diag"
-if [[ ! -d $diag_dir ]]; then
-  exit 6
+
+if [[ ! -d "$diag_dir" ]]; then
+    exit 6
 fi
 
-fileEnd=".*_[0-9]+\."
-trcTrm="${fileEnd}(trc|trm)$"
-log="${fileEnd}log$"
-xml="${fileEnd}xml$"
+machineName=$(hostname -s)
 
-crsPath="diag_dir/crs/$machine/crs/trace"
-crsFiles=$(find $crsPath -type f -mtime +$1 -printf '%s %p\n' |egrep "$trcTrm | cut -d ' ' -f1)
-size=$(echo "$crsFiles | awk '{ sum += $0} END { print sum/1024}')
-echo "CRS: $size"
+sum_size() {
+    total=0
 
-tnslsnrPath="$diag_dir/tnslsnr/$machine"
-tnslsnrFiles=$(find $tnslsnrPath -mindepth 3 -maxdepth 3 -type f -path "$tnslsnrPath/*/alert/*" -o -path "$tnslsnrPath/*/trace/*" -mtime +$1 -printf '%s %p\n' | egrep "(alert/$xml|trace/$log)" | cut -d ' ' -f 1)
-size=$(echo "$tnslsnrFiles" | awk '{ sum += $0 } END {print sum/1024}')
+    while read -r file; do
+        size=$(stat -c '%s' "$file")
+        total=$((total + size))
+    done
 
-echo "TNSLSNR: $size"
+    echo $((total / 1024))
+}
 
-rdbmsPath="$diag_dir/rdbms"
-rdbmsFiles=$(find $rdbmsPath -mindepth 2 -maxdepth 2 -type f -mtime +$1 -printf '%s %p\n' | egrep "$trcTrm" | cut -d ' ' -f 1)
-size=$(echo "$rdbmsFiles" | awk '{ sum += $0 } END {print sum/1024}')
+if [[ "$user" == "oracle" ]]; then
 
-echo "RDBMS: $size"
+    dirrdbms="$diag_dir/rdbms"
+
+    if [[ ! -d "$dirrdbms" ]]; then
+        exit 7
+    fi
+
+    rdbms=$(
+        find "$dirrdbms" \
+            -mindepth 3 -maxdepth 3 \
+            -type f \
+            -mtime +"$days" \
+            \( -name '*_[0-9]*.trc' -o -name '*_[0-9]*.trm' \) |
+        sum_size
+    )
+
+    echo "rdbms: $rdbms"
+
+elif [[ "$user" == "grid" ]]; then
+
+    dirCRS="$diag_dir/crs/$machineName/crs/trace"
+
+    if [[ ! -d "$dirCRS" ]]; then
+        exit 8
+    fi
+
+    crs=$(
+        find "$dirCRS" \
+            -type f \
+            -mtime +"$days" \
+            \( -name '*_[0-9]*.trc' -o -name '*_[0-9]*.trm' \) |
+        sum_size
+    )
+
+    tnslsnr=$(
+        find "$diag_dir/tnslsnr/$machineName" \
+            -type f \
+            -mtime +"$days" \
+            \( \
+                \( -path '*/alert/*' -name '*_[0-9]*.xml' \) -o \
+                \( -path '*/trace/*' -name '*_[0-9]*.log' \) \
+            \) |
+        sum_size
+    )
+
+    echo "crs: $crs"
+    echo "tnslsnr: $tnslsnr"
+fi
