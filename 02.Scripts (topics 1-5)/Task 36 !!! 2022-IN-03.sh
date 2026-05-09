7dd50a294117cfb4f6518a69705d252325a12c0a
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-09 22:03:07 +0300

    Update Task 36 !!! 2022-IN-03.sh

--- 02.Scripts (topics 1-5)/Task 36 !!! 2022-IN-03.sh
+++ 02.Scripts (topics 1-5)/Task 36 !!! 2022-IN-03.sh
@@ -1,71 +1,44 @@
 #!/bin/bash
 
-if [[ $# -lt 2 ]]; then
-  echo "Error" >&2
-  exit 1
+if [[ $# -eq 0 ]]; then
+    exit 1
 fi
 
-jar=""
-jarFile=""
+options=""
+args=""
+jar_file=""
+seen_jar=0
+found_file=0
 
-options="$(mktemp)"
-args="$(mktemp)"
-trap 'rm -f "$options" "$args"' EXIT
+while [[ $# -gt 0 ]]; do
+    x="$1"
+    shift
 
-for arg in "$@"; do
-  # 1) Ако вече имаме jarFile -> всичко след него са args
-  if [[ -n "$jarFile" ]]; then
-    echo "$arg" >> "$args"
-    continue
-  fi
-
-  # 2) Ако започва с '-' -> опция/флаг
-  if [[ "$arg" =~ ^- ]]; then
-    # срещнахме -jar
-    if [[ "$arg" == "-jar" ]]; then
-      jar="$arg"
-      continue
+    if [[ "$x" == "-jar" ]]; then
+        seen_jar=1
+        continue
     fi
 
-    # записваме като option (логиката ти е такава)
-    echo "$arg" >> "$options"
-
-    # специално правило за -D...
-    if [[ "$arg" =~ ^-D ]]; then
-      if [[ -n "$jar" ]]; then
-        : # ок, вече е записано в options
-      else
-        echo "Error -Dprop=val can't be passed before -jar" >&2
-        exit 1
-      fi
+    if [[ "$found_file" -eq 1 ]]; then
+        args="$args $x"
+        continue
     fi
 
-  # 3) Не започва с '-' -> може да е jarFile (само ако вече е срещнат -jar)
-  else
-    if [[ -n "$jar" ]]; then
-      jarFile="$arg"
-      # не го пишем в args (jarFile не е аргумент за main)
+    if [[ "$seen_jar" -eq 0 ]]; then
+        options="$options $x"
+        continue
     fi
-  fi
+
+    if [[ "$x" == -* ]]; then
+        options="$options $x"
+    else
+        jar_file="$x"
+        found_file=1
+    fi
 done
 
-# Проверки, че имаме -jar и jarFile
-if [[ -z "$jar" ]]; then
-  echo "Error: missing -jar" >&2
-  exit 1
+if [[ -z "$jar_file" ]]; then
+    exit 2
 fi
 
-if [[ -z "$jarFile" ]]; then
-  echo "Error: jar file not found after -jar" >&2
-  exit 1
-fi
-
-# Събираме опциите и аргументите на 1 ред (по твоята идея)
-opts="$(tr '\n' ' ' < "$options" | sed 's/[[:space:]]*$//')"
-arguments="$(tr '\n' ' ' < "$args" | sed 's/[[:space:]]*$//')"
-
-# ВАЖНО: правилният стандартен синтаксис е:
-# java [options] -jar jarFile [args]
-# (ти си държал options отделно, args отделно -> така го изпълняваме)
-java $opts -jar "$jarFile" $arguments
-exit $?
+java $options -jar "$jar_file" $args
