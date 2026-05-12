4e090ccb46dfb17c1a37285a20678125d4334f1c
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-12 22:00:18 +0300

    Update Task 46 !!!  2023-SE-03.sh

--- 02.Scripts (topics 1-5)/Task 46 !!!  2023-SE-03.sh
+++ 02.Scripts (topics 1-5)/Task 46 !!!  2023-SE-03.sh
@@ -1,43 +1,74 @@
 #!/bin/bash
 
 if [[ $# -ne 1 ]]; then
-    echo "error"
+    echo "Usage: $0 <dir>" >&2
     exit 1
 fi
 
-if [[ ! -d "$1" ]]; then
-    echo "error"
+dir="$1"
+
+if [[ ! -d "$dir" ]]; then
+    echo "Invalid directory" >&2
     exit 1
 fi
 
-words=$(mktemp)
+files_count=$(find "$dir" -type f | wc -l)
 
-allFiles=$(find "$1" -mindepth 1 -type f | wc -l)
-
-if [[ "$allFiles" -eq 0 ]]; then
-    rm -f "$words"
+if [[ "$files_count" -eq 0 ]]; then
     exit 0
 fi
 
-while read -r file;do
-      tr -cs  'a-z' '\n'  < "$file"  #всичко което НЕ е буква a-z → става нов ред
-      | sed '/^$/d' |  sort | uniq -c     #махни празните редове
-      | tr -s ' ' | sed 's/^//'  |grep -E '^[0-9]+ [a-z]+$' >>"$words"     #<spaces><count><space><word>
-done< <(find "$1" -mindepth 1 -type f)
+needed=$(( (files_count + 1) / 2 ))
 
-allFilesWords=$(mktemp)
-awk '{ count[$2] += $1} END {for (word in count) print word, count[word] }' "$words" > "$allFilesWords"
-uniqWords=$(mktemp)
-cut -d '' -f2 "$words" | sort | uniw > "$uniqWords"
-res=$(mktemp)
+tmp=$(mktemp)
 
-while read -r word;do  
-  fileOccurs=$(awk -v w="$word" '$2 == w && $1 >=3 {c++} END {print c+0}' "$words")   #в колко файла тази дума се среща поне 3 пъти
-  sum=$(awk -v w="$word" '$1 ==w {print $2}' "$allFilesWords")
-  if [[ $((fileOccurs *2 )) -ge "$allFiles" ]];then
-    echo "$word $sum" >> "$res"
+trap 'rm -f "$tmp"' EXIT
+
+find "$dir" -type f | while read -r file; do
+
+    tr -c 'a-z' '\n' < "$file" |
+    grep -v '^$' |
+    sort |
+    uniq -c |
+
+    while read -r count word; do
+        echo "$word $count $file"
+    done
+
+done > "$tmp"
+
+cut -d ' ' -f1 "$tmp" | sort | uniq |
+
+while read -r word; do
+
+    total=0
+
+    while read -r count; do
+        total=$(( total + count ))
+    done < <(
+        grep "^$word " "$tmp" |
+        cut -d ' ' -f2
+    )
+
+    good_files=0
+
+    while read -r count; do
+
+        if [[ "$count" -ge 3 ]]; then
+            good_files=$(( good_files + 1 ))
+        fi
+
+    done < <(
+        grep "^$word " "$tmp" |
+        cut -d ' ' -f2
+    )
+
+    if [[ "$good_files" -ge "$needed" ]]; then
+        echo "$total $word"
     fi
-done < "$uniqWords"
 
-sort -k2,2 nr -k1,1 "$res |  head -n10 | cut -d ' ' -f1
-rm -f "$words" "$allFilesWords" "$uniqWords" "$res"
+done |
+
+sort -nr |
+head -n 10 |
+cut -d ' ' -f2
