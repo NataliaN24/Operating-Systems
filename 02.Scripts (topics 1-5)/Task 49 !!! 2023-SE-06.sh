543645e80a56003beb1b5ea28723b6964ae840b1
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-13 22:27:55 +0300

    Update Task 49 !!! 2023-SE-06.sh

--- 02.Scripts (topics 1-5)/Task 49 !!! 2023-SE-06.sh
+++ 02.Scripts (topics 1-5)/Task 49 !!! 2023-SE-06.sh
@@ -1,6 +1,116 @@
 #!/bin/bash
 
 if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 CAMERA_DIR LIBRARY_DIR"
+    exit 1
+fi
+
+camera="$1"
+library="$2"
+
+if [[ ! -d "$camera" ]]; then
+    echo "Camera directory does not exist"
+    exit 1
+fi
+
+if [[ -e "$library" ]]; then
+    echo "Library directory already exists"
+    exit 1
+fi
+
+mkdir "$library"
+
+photos=$(mktemp)
+dates=$(mktemp)
+map=$(mktemp)
+
+find "$camera" -type f -name "*.jpg" | while read -r file; do
+    ts=$(stat -c '%Y' "$file")
+
+    day=$(date -d "@$ts" +'%Y-%m-%d')
+
+    newname=$(date -d "@$ts" +'%Y-%m-%d_%H:%M:%S.jpg')
+
+    echo "$ts $day $newname $file"
+
+done | sort -n > "$photos"
+
+cut -d ' ' -f2 "$photos" | sort -u > "$dates"
+
+start=""
+prev=""
+
+while read -r day; do
+
+    if [[ "$start" == "" ]]; then
+
+        start="$day"
+        prev="$day"
+
+    else
+
+        next=$(date -d "$prev + 1 day" +'%Y-%m-%d')
+
+        if [[ "$day" == "$prev" ]]; then
+
+            continue
+
+        elif [[ "$day" == "$next" ]]; then
+
+            prev="$day"
+
+        else
+
+            interval="${start}_${prev}"
+
+            d="$start"
+
+            while [[ "$d" < "$prev" || "$d" == "$prev" ]]; do
+
+                echo "$d $interval" >> "$map"
+
+                d=$(date -d "$d + 1 day" +'%Y-%m-%d')
+
+            done
+
+            start="$day"
+            prev="$day"
+
+        fi
+    fi
+
+done < "$dates"
+
+if [[ "$start" != "" ]]; then
+
+    interval="${start}_${prev}"
+
+    d="$start"
+
+    while [[ "$d" < "$prev" || "$d" == "$prev" ]]; do
+
+        echo "$d $interval" >> "$map"
+
+        d=$(date -d "$d + 1 day" +'%Y-%m-%d')
+
+    done
+fi
+
+while read -r ts day newname file; do
+
+    interval=$(grep "^$day " "$map" | cut -d ' ' -f2)
+
+    mkdir -p "$library/$interval"
+
+    cp "$file" "$library/$interval/$newname"
+
+done < "$photos"
+
+rm -f "$photos" "$dates" "$map"
+#########################################################################################
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
     echo "error"
     exit 1
 fi
