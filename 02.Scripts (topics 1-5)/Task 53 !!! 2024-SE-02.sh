c49c962663a0badebd95ba7ea0883a3d13f9a663
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-14 21:23:07 +0300

    Update Task 53 !!! 2024-SE-02.sh

--- 02.Scripts (topics 1-5)/Task 53 !!! 2024-SE-02.sh
+++ 02.Scripts (topics 1-5)/Task 53 !!! 2024-SE-02.sh
@@ -1,69 +1,63 @@
 #!/bin/bash
 
-if [[ ${#} -ne 0 ]];
-then
-    echo "error"
-    exit 1
-fi
-
-# passwd файл
-if [[ -n "$PASSWD" ]];
-then
-    passwdFile="$PASSWD"
+if [[ -z "$PASSWD" ]]; then
+    passwd_file="/etc/passwd"
 else
-    passwdFile="/etc/passwd"
+    passwd_file="$PASSWD"
 fi
 
-SCRIPT_DIR=$(dirname "$0")   #програмата occ се намира в същата директория като скрипта
-users=$(
-    awk -F: '$3 >= 1000 { print $1 }' "$passwdFile" | sort
-)
-localsNotInPC=$(comm -23 <(echo "$users") <(echo "$prevCloudUsers"))
-PCNotInLocal=$(comm -13 <(echo "$users") <(echo "$prevCloudUsers"))
-mutual=$(comm -12 <(echo "$users") <(echo "$prevCloudUsers"))
+script_dir=$(dirname "$0")
+occ="$script_dir/occ"
 
-while read -r user;
-do
-    if [[ -n "$user" ]];
-    then
-        "$SCRIPT_DIR"/occ user:add "$user"
+local_users=$(mktemp)
+cloud_users=$(mktemp)
+
+# взимаме локалните потребители
+cat "$passwd_file" | while IFS=':' read username pass uid gid gecos home shell; do
+    if [[ "$uid" -ge 1000 ]]; then
+        echo "$username"
     fi
-done < <(echo "$localsNotInPC")
+done | sort > "$local_users"
 
-while read -r user;
-do
-    if [[ -n "$user" ]];
-    then
-        info=$(
-            "$SCRIPT_DIR"/occ user:info "$user" |
-            grep "enabled:" |
-            cut -d: -f2 |
-            tr -d ' '
-        )
+# взимаме PrevCloud потребителите
+"$occ" user:list | while read line; do
+    user=$(echo "$line" | cut -d ':' -f1 | sed 's/- //')
+    echo "$user"
+done | sort > "$cloud_users"
 
-        if [[ "$info" == "false" ]];
-        then
-            "$SCRIPT_DIR"/occ user:enable "$user"
+# локален потребител, който го няма в cloud
+cat "$local_users" | while read user; do
+
+    found=$(grep "^$user$" "$cloud_users")
+
+    if [[ -z "$found" ]]; then
+        "$occ" user:add "$user"
+    else
+
+        enabled=$("$occ" user:info "$user" | grep "enabled:" | cut -d ' ' -f2)
+
+        if [[ "$enabled" == "false" ]]; then
+            "$occ" user:enable "$user"
         fi
     fi
-done < <(echo "$mutual")
 
-while read -r user;
-do
-    if [[ -n "$user" ]];
-    then
-        info=$(
-            "$SCRIPT_DIR"/occ user:info "$user" |
-            grep "enabled:" |
-            cut -d: -f2 |
-            tr -d ' '
-        )
+done
 
-        if [[ "$info" == "true" ]];
-        then
-            "$SCRIPT_DIR"/occ user:disable "$user"
+# cloud потребител, който го няма локално
+cat "$cloud_users" | while read user; do
+
+    found=$(grep "^$user$" "$local_users")
+
+    if [[ -z "$found" ]]; then
+
+        enabled=$("$occ" user:info "$user" | grep "enabled:" | cut -d ' ' -f2)
+
+        if [[ "$enabled" == "true" ]]; then
+            "$occ" user:disable "$user"
         fi
     fi
-done < <(echo "$PCNotInLocal")
 
-exit 0
+done
+
+rm "$local_users"
+rm "$cloud_users"
