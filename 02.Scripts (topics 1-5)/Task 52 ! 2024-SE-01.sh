3991723724c394de97a415b997b7b69e4eae1b49
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-14 20:51:57 +0300

    Update Task 52 ! 2024-SE-01.sh

--- 02.Scripts (topics 1-5)/Task 52 ! 2024-SE-01.sh
+++ 02.Scripts (topics 1-5)/Task 52 ! 2024-SE-01.sh
@@ -1,5 +1,73 @@
 #!/bin/bash
 
+if [[ $# -eq 0 ]]; then
+    echo "No arguments"
+    exit 1
+fi
+
+files=$(mktemp)
+rules=$(mktemp)
+markers=$(mktemp)
+
+for arg in "$@"; do
+
+    if [[ "$arg" == -R*=* ]]; then
+        echo "$arg" >> "$rules"
+
+    elif [[ "$arg" != -* ]]; then
+        if [[ ! -f "$arg" ]]; then
+            echo "Invalid file: $arg"
+            exit 1
+        fi
+
+        echo "$arg" >> "$files"
+
+    else
+        echo "Invalid argument: $arg"
+        exit 1
+    fi
+
+done
+
+if [[ ! -s "$files" || ! -s "$rules" ]]; then
+    echo "Need at least one file and one replacement"
+    exit 1
+fi
+
+while read -r rule; do
+    left=$(echo "$rule" | sed 's/^-R//' | cut -d '=' -f1)
+    right=$(echo "$rule" | cut -d '=' -f2)
+
+    marker=$(pwgen 32 1)
+
+    echo "$left=$right=$marker" >> "$markers"
+
+done < "$rules"
+
+while read -r file; do
+
+    while read -r line; do
+        old=$(echo "$line" | cut -d '=' -f1)
+        marker=$(echo "$line" | cut -d '=' -f3)
+
+        sed -i -E "s/\b$old\b/$marker/g" "$file"
+
+    done < "$markers"
+
+    while read -r line; do
+        new=$(echo "$line" | cut -d '=' -f2)
+        marker=$(echo "$line" | cut -d '=' -f3)
+
+        sed -i -E "s/\b$marker\b/$new/g" "$file"
+
+    done < "$markers"
+
+done < "$files"
+
+rm "$files" "$rules" "$markers"
+###########################################################################################
+#!/bin/bash
+
 rules=$(mktemp)
 files=$(mktemp)
 
