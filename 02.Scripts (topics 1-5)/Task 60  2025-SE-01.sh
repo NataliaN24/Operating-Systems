7f7697b1c0808eaac060fa39a0516d6f1235acbb
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-16 20:57:59 +0300

    Update Task 60  2025-SE-01.sh

--- 02.Scripts (topics 1-5)/Task 60  2025-SE-01.sh
+++ 02.Scripts (topics 1-5)/Task 60  2025-SE-01.sh
@@ -1,3 +1,55 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 <config> <input>"
+    exit 1
+fi
+
+config="$1"
+input="$2"
+
+if [[ ! -f "$config" || ! -f "$input" ]]; then
+    echo "Invalid file"
+    exit 1
+fi
+
+filename=$(basename "$input")
+withoutExtension="${filename%.*}"
+
+while read -r line; do
+    lang=$(echo "$line" | cut -d ' ' -f1)
+
+    dirBase=$(echo "$line" | grep -o "'[^']*'")
+    dir="${dirBase%\'}"
+    dir="${dir#\'}"
+
+    isVisitor=false
+    isListener=false
+
+    echo "$line" | grep -q "visitor"
+    if [[ $? -eq 0 ]]; then
+        isVisitor=true
+    fi
+
+    echo "$line" | grep -q "listener"
+    if [[ $? -eq 0 ]]; then
+        isListener=true
+    fi
+
+    opts=""
+
+    if [[ "$isListener" == "false" ]]; then
+        opts="$opts -no-listener"
+    fi
+
+    if [[ "$isVisitor" == "true" ]]; then
+        opts="$opts -visitor"
+    fi
+
+    antlr4 -Dlanguage="$lang" $opts -o "$dir/$withoutExtension" "$input"
+
+done < "$config"
+####################################################################################
 #/bin/bash
 
 if [[ $# -ne 2 ]];then
