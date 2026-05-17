b0a64f9bf4fd23b1a11f6866d54f861417f46023
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-17 17:53:44 +0300

    Update Task 61 (PROCESS)!!! 2025-SE-02.sh

--- 02.Scripts (topics 1-5)/Task 61 (PROCESS)!!! 2025-SE-02.sh
+++ 02.Scripts (topics 1-5)/Task 61 (PROCESS)!!! 2025-SE-02.sh
@@ -1,5 +1,122 @@
 #!/bin/bash
 
+if [[ -z "$SVC_DIR" || ! -d "$SVC_DIR" ]]; then
+    exit 1
+fi
+
+[[ "$#" -lt 1 ]] && exit 1
+
+command="$1"
+
+if [[ "$command" == "start" ]]; then
+
+    [[ "$#" -ne 2 ]] && exit 1
+
+    service="$2"
+    service_file=""
+
+    for file in "$SVC_DIR"/*; do
+        [[ -f "$file" ]] || continue
+
+        name=$(grep "^name:" "$file" | cut -d ' ' -f2)
+
+        if [[ "$name" == "$service" ]]; then
+            service_file="$file"
+            break
+        fi
+    done
+
+    [[ -z "$service_file" ]] && exit 1
+
+    pidfile=$(grep "^pidfile:" "$service_file" | cut -d ' ' -f2)
+    outfile=$(grep "^outfile:" "$service_file" | cut -d ' ' -f2)
+    comm=$(grep "^comm:" "$service_file" | cut -d ' ' -f2-)
+
+    running=0
+
+    if [[ -f "$pidfile" ]]; then
+        pid=$(cat "$pidfile")
+
+        if ps -p "$pid" > /dev/null 2>&1; then
+            running=1
+        fi
+    fi
+
+    if [[ "$running" -eq 0 ]]; then
+        bash -c "$comm" > "$outfile" 2>&1 &
+        echo "$!" > "$pidfile"
+    fi
+
+elif [[ "$command" == "stop" ]]; then
+
+    [[ "$#" -ne 2 ]] && exit 1
+
+    service="$2"
+    service_file=""
+
+    for file in "$SVC_DIR"/*; do
+        [[ -f "$file" ]] || continue
+
+        name=$(grep "^name:" "$file" | cut -d ' ' -f2)
+
+        if [[ "$name" == "$service" ]]; then
+            service_file="$file"
+            break
+        fi
+    done
+
+    [[ -z "$service_file" ]] && exit 1
+
+    pidfile=$(grep "^pidfile:" "$service_file" | cut -d ' ' -f2)
+
+    if [[ -f "$pidfile" ]]; then
+        pid=$(cat "$pidfile")
+        kill -TERM "$pid" 2>/dev/null
+    fi
+
+elif [[ "$command" == "running" ]]; then
+
+    [[ "$#" -ne 1 ]] && exit 1
+
+    for file in "$SVC_DIR"/*; do
+        [[ -f "$file" ]] || continue
+
+        name=$(grep "^name:" "$file" | cut -d ' ' -f2)
+        pidfile=$(grep "^pidfile:" "$file" | cut -d ' ' -f2)
+
+        if [[ -f "$pidfile" ]]; then
+            pid=$(cat "$pidfile")
+
+            if ps -p "$pid" > /dev/null 2>&1; then
+                echo "$name"
+            fi
+        fi
+    done | sort
+
+elif [[ "$command" == "cleanup" ]]; then
+
+    [[ "$#" -ne 1 ]] && exit 1
+
+    for file in "$SVC_DIR"/*; do
+        [[ -f "$file" ]] || continue
+
+        pidfile=$(grep "^pidfile:" "$file" | cut -d ' ' -f2)
+
+        if [[ -f "$pidfile" ]]; then
+            pid=$(cat "$pidfile")
+
+            if ! ps -p "$pid" > /dev/null 2>&1; then
+                rm -- "$pidfile"
+            fi
+        fi
+    done
+
+else
+    exit 1
+fi
+###############################################################################################################
+#!/bin/bash
+
 if [[ -z "$SVC_DIR" ]]; then
     exit 1
 fi
