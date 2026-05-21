fec1df004c43ad0f9887876bbcb8c46ca3028f13
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-21 21:48:26 +0300

    Update Task 08  2017-SE-04.c

--- 03.C/Task 08  2017-SE-04.c
+++ 03.C/Task 08  2017-SE-04.c
@@ -10,6 +10,59 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <err.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    char buff[4096];
+    ssize_t bytesR;
+
+    if (argc == 1) {
+        while ((bytesR = read(0, buff, sizeof(buff))) > 0) {
+            if (write(1, buff, bytesR) != bytesR) {
+                err(1, "write");
+            }
+        }
+
+        if (bytesR == -1) {
+            err(1, "read");
+        }
+    } else {
+        for (int i = 1; i < argc; i++) {
+            int fd;
+
+            if (argv[i][0] == '-') {
+                fd = 0;
+            } else {
+                fd = open(argv[i], O_RDONLY);
+                if (fd == -1) {
+                    err(1, "open");
+                }
+            }
+
+            while ((bytesR = read(fd, buff, sizeof(buff))) > 0) {
+                if (write(1, buff, bytesR) != bytesR) {
+                    err(1, "write");
+                }
+            }
+
+            if (bytesR == -1) {
+                err(1, "read");
+            }
+
+            if (fd != 0) {
+                close(fd);
+            }
+        }
+    }
+
+    exit(0);
+}
+////////////////////////////////////////////////////////////////////////////////////
 //$ ./main f - g
 
 void copyToStdout(int fd)
