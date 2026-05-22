b9f8ee1991af20c2a6e3cded2bae829a2e4fc682
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-22 23:07:17 +0300

    Update Task 18 2021-SE-03.c

--- 03.C/Task 18 2021-SE-03.c
+++ 03.C/Task 18 2021-SE-03.c
@@ -1,3 +1,122 @@
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <err.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+void writeStr(int fd, const char* str)
+{
+    int len = 0;
+
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    if (write(fd, str, len) < 0) {
+        err(1, "write");
+    }
+}
+
+void writeNum(int fd, uint32_t num)
+{
+    char buf[20];
+
+    int i = 0;
+
+    if (num == 0) {
+        buf[i++] = '0';
+    }
+
+    while (num > 0) {
+        buf[i++] = (num % 10) + '0';
+        num /= 10;
+    }
+
+    for (int j = i - 1; j >= 0; j--) {
+        if (write(fd, &buf[j], 1) < 0) {
+            err(1, "write");
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3) {
+        errx(1, "usage");
+    }
+
+    int fdIn = open(argv[1], O_RDONLY);
+    if (fdIn < 0) {
+        err(1, "open input");
+    }
+
+    struct stat st;
+
+    if (fstat(fdIn, &st) < 0) {
+        err(1, "stat");
+    }
+
+    if (st.st_size % sizeof(uint16_t) != 0) {
+        errx(1, "invalid file");
+    }
+
+    uint32_t count = st.st_size / sizeof(uint16_t);
+
+    if (count > 524288) {
+        errx(1, "too many elements");
+    }
+
+    int fdOut = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (fdOut < 0) {
+        err(1, "open output");
+    }
+
+    writeStr(fdOut, "#include <stdint.h>\n\n");
+    writeStr(fdOut, "const uint16_t arr[] = {\n");
+
+    uint16_t num;
+
+    for (uint32_t i = 0; i < count; i++) {
+
+        ssize_t r = read(fdIn, &num, sizeof(num));
+
+        if (r < 0) {
+            err(1, "read");
+        }
+
+        if (r != sizeof(num)) {
+            errx(1, "broken file");
+        }
+
+        writeStr(fdOut, "    ");
+
+        writeNum(fdOut, num);
+
+        if (i + 1 != count) {
+            writeStr(fdOut, ",");
+        }
+
+        writeStr(fdOut, "\n");
+    }
+
+    writeStr(fdOut, "};\n\n");
+    writeStr(fdOut, "const uint32_t arrN = ");
+
+    writeNum(fdOut, count);
+
+    writeStr(fdOut, ";\n");
+
+    close(fdIn);
+    close(fdOut);
+
+    exit(0);
+}
+
+
+////////////////////////////////////////////////////////////
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdint.h>
