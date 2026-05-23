1877ff11dd70c0561d78fef11af5163075d86978
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-23 14:17:43 +0300

    Update Task 22  2023-SE-01.c

--- 03.C/Task 22  2023-SE-01.c
+++ 03.C/Task 22  2023-SE-01.c
@@ -1,3 +1,82 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <err.h>
+
+int main(int argc, char* argv[])
+{
+    if(argc != 3){
+        errx(1, "Usage: ./main stream.bin messages.bin");
+    }
+
+    int in = open(argv[1], O_RDONLY);
+    if(in < 0){
+        err(2, "open input");
+    }
+
+    int out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+    if(out < 0){
+        err(3, "open output");
+    }
+
+    uint8_t b;
+
+    while(read(in, &b, 1) == 1)
+    {
+        if(b != 0x55){
+            continue;
+        }
+
+        uint8_t len;
+
+        if(read(in, &len, 1) != 1){
+            break;
+        }
+
+        if(len < 3){
+            continue;
+        }
+
+        uint8_t msg[256];
+
+        msg[0] = 0x55;
+        msg[1] = len;
+
+        uint8_t checksum = msg[0] ^ msg[1];
+
+        int ok = 1;
+
+        for(uint16_t i = 2; i < len; i++)
+        {
+            if(read(in, &msg[i], 1) != 1){
+                ok = 0;
+                break;
+            }
+
+            if(i < len - 1){
+                checksum ^= msg[i];
+            }
+        }
+
+        if(!ok){
+            break;
+        }
+
+        if(checksum == msg[len - 1]){
+            if(write(out, msg, len) != len){
+                err(4, "write");
+            }
+        }
+    }
+
+    close(in);
+    close(out);
+
+    exit(0);
+}
+///////////////////////////////////////////////
+
 #include <fcntl.h>
 #include <err.h>
 #include <unistd.h>
