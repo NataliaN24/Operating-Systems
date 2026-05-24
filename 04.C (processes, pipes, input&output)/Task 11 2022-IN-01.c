ee4eaf592cdddeb3ac266b50484257cbc683adea
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-24 19:34:09 +0300

    Update Task 11 2022-IN-01.c

--- 04.C (processes, pipes, input&output)/Task 11 2022-IN-01.c
+++ 04.C (processes, pipes, input&output)/Task 11 2022-IN-01.c
@@ -1,90 +1,93 @@
-
 #include <fcntl.h>
 #include <err.h>
 #include <unistd.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
-#include <time.h>
-#include <sys/time.h>
-#include <pwd.h>
 
 const char DING[] = "DING ";
-const char DONG[] = "DONG ";
-char c = 'a';
+const char DONG[] = "DONG\n";
 
 int main(int argc, char* argv[])
 {
-   if(argc!=3)
-   {
-     err(1,"error");
-   }
-   int n=*argv[1]-'0';
-   int d=*argv[2]-'0';
-   
-   int parentToChild[2];
-   if(pipe(pipe1)<0)
-   {
-      err(1,"error");
-   }
-   int childToParent[2];
-   if(pipe(pip2)<0)
-   {
-      err(1,"error");
-   }
-   int child=fork();
-   if(child<0){err(1,"error");}
-   if(child==0)
-   {
-     close(parentToChild[1]);
-     close(childToParent[0]);
-     char x;
-     for(long i=0;i<n;i++)
-     {
-          if(read(parentToChild[0],&x,1)!=1)
-          {
-                 err(1, "child read");
-          }
-          if(write(1,DONG,5)!=5)
-          {
-                 err(1, "child read");
-          }
-          if(write(childToParent[1],"x",1)!=1)
-          {
-                 err(1, "child read");
-          }
-     }
-     close(parentToChild[0]);
-     close(childToParent[1]);
-     _exit(0);
-   }
-   //parent
+    if (argc != 3) {
+        errx(1, "invalid args");
+    }
 
-   close(parentToChild[0]);
-   close(childToParent[1]);
-   char x;
-   for(long i=0;i<n;i++)
-   {
-     if(write(1,DING,5)!=5)
-     {
-            err(1, "child read");
-     }
-     if(write(parentToChild[1],"x",1)!=1)
-     {
-            err(1, "child read");
-     }
-     if(read(childToParent[0],&x,1)!=1)
-     {
-            err(1, "child read");
-     }
-     sleep((unsigned int)D);
-   }
-     close(p2c[1]);
-    close(c2p[0]);
+    if (strlen(argv[1]) != 1 || strlen(argv[2]) != 1) {
+        errx(1, "args must be one digit");
+    }
+
+    if (*argv[1] < '1' || *argv[1] > '9' || *argv[2] < '1' || *argv[2] > '9') {
+        errx(1, "args must be digits 1-9");
+    }
+
+    int n = *argv[1] - '0';
+    int d = *argv[2] - '0';
+
+    int parentToChild[2];
+    if (pipe(parentToChild) < 0) {
+        err(1, "pipe");
+    }
+
+    int childToParent[2];
+    if (pipe(childToParent) < 0) {
+        err(1, "pipe");
+    }
+
+    pid_t child = fork();
+    if (child < 0) {
+        err(1, "fork");
+    }
+
+    if (child == 0) {
+        close(parentToChild[1]);
+        close(childToParent[0]);
+
+        char x;
+
+        for (int i = 0; i < n; i++) {
+            if (read(parentToChild[0], &x, 1) != 1) {
+                err(1, "read");
+            }
+
+            if (write(1, DONG, 5) != 5) {
+                err(1, "write");
+            }
+
+            if (write(childToParent[1], "x", 1) != 1) {
+                err(1, "write");
+            }
+        }
+
+        close(parentToChild[0]);
+        close(childToParent[1]);
+        _exit(0);
+    }
+
+    close(parentToChild[0]);
+    close(childToParent[1]);
+
+    char x;
+
+    for (int i = 0; i < n; i++) {
+        if (write(1, DING, 5) != 5) {
+            err(1, "write");
+        }
+
+        if (write(parentToChild[1], "x", 1) != 1) {
+            err(1, "write");
+        }
+
+        if (read(childToParent[0], &x, 1) != 1) {
+            err(1, "read");
+        }
+
+        sleep(d);
+    }
+
+    close(parentToChild[1]);
+    close(childToParent[0]);
 
     int status;
     if (wait(&status) < 0) {
@@ -92,9 +95,11 @@ int main(int argc, char* argv[])
     }
 
     if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-        errx(1, "child did not exit successfully");
+        errx(1, "child failed");
     }
 
+    return 0;
+}
     return 0;
 
 }
