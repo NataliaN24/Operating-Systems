64f9a68e6b3069c1c40e385278d9b401954eee93
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-05-30 13:32:48 +0300

    Update and rename Task 12 !!! 2023-IN-01.c to Task 12 !!!! 2023-IN-01.c

--- 04.C (processes, pipes, input&output)/Task 12 !!!! 2023-IN-01.c
+++ 04.C (processes, pipes, input&output)/Task 12 !!!! 2023-IN-01.c
@@ -1,3 +1,124 @@
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <err.h>
+
+int main(int argc, char* argv[])
+{
+    if(argc != 3)
+    {
+        errx(26, "Invalid arguments");
+    }
+
+    int NC = strtol(argv[1], NULL, 10);
+    int WC = strtol(argv[2], NULL, 10);
+
+    if(NC < 1 || NC > 7 || WC < 1 || WC > 35)
+    {
+        errx(26, "Invalid arguments");
+    }
+
+    int total = NC + 1;
+
+    int pipes[total][2];
+    pid_t pids[NC];
+
+    for(int i = 0; i < total; i++)
+    {
+        if(pipe(pipes[i]) < 0)
+        {
+            err(26, "pipe");
+        }
+    }
+
+    int id = 0;
+
+    for(int i = 1; i < total; i++)
+    {
+        pid_t pid = fork();
+
+        if(pid < 0)
+        {
+            err(26, "fork");
+        }
+
+        if(pid == 0)
+        {
+            id = i;
+            break;
+        }
+
+        pids[i - 1] = pid;
+    }
+
+    char* words[3] =
+    {
+        "tic ",
+        "tac ",
+        "toe\n"
+    };
+
+    while(1)
+    {
+        int num;
+
+        if(read(pipes[id][0], &num, sizeof(num)) < 0)
+        {
+            err(26, "read");
+        }
+
+        if(num >= WC)
+        {
+            int next = (id + 1) % total;
+
+            write(pipes[next][1], &num, sizeof(num));
+
+            break;
+        }
+
+        if(write(1, words[num % 3], 4) < 0)
+        {
+            err(26, "write");
+        }
+
+        num++;
+
+        int next = (id + 1) % total;
+
+        if(write(pipes[next][1], &num, sizeof(num)) < 0)
+        {
+            err(26, "write");
+        }
+    }
+
+    if(id == 0)
+    {
+        int start = 0;
+
+        if(write(pipes[0][1], &start, sizeof(start)) < 0)
+        {
+            err(26, "write");
+        }
+
+        for(int i = 0; i < NC; i++)
+        {
+            waitpid(pids[i], NULL, 0);
+        }
+    }
+
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdint.h>
