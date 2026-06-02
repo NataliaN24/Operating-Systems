16b15f771f73005fad5572bcce7bf2c1ffb6e09a
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-06-02 22:51:23 +0300

    Update Task 04 2017-IN-03.sh

--- 01.Stream Processing (topics 1,2,3)/Task 04 2017-IN-03.sh
+++ 01.Stream Processing (topics 1,2,3)/Task 04 2017-IN-03.sh
@@ -7,3 +7,8 @@ sort |
 uniq -c |
 sort -nr |
 head -n1
+
+
+
+###################################################
+ grep home/Inf/ test2017 | cut -d : -f1,5 | tr ':' ' '  | tr ',' ' ' | cut -d ' ' -f1,3 | egrep '.*a$' | cut -d ' ' -f1 | cut  -c 2-3 | sort | uniq -c | sort -nr | tr -s ' ' | sed 's/ //' | head -n 1 | cut -d ' ' -f2
