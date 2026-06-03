35319ae00632ba1a6ec4efc63d8174f5dc66d96b
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-06-03 23:17:34 +0300

    Update Task 10  2017-SE-01.sh

--- 02.Scripts (topics 1-5)/Task 10  2017-SE-01.sh
+++ 02.Scripts (topics 1-5)/Task 10  2017-SE-01.sh
@@ -11,6 +11,32 @@ destination (счупени symlinkове).
 #!/bin/bash
 
 if [[ $# -lt 1 || $# -gt 2 ]]; then
+    exit 1
+fi
+
+if [[ ! -d "$1" ]]; then
+    exit 1
+fi
+
+if [[ $# -eq 2 ]]; then
+    find "$1" -type f -printf '%n %p\n' | while read -r hardlink path; do
+        if [[ "$hardlink" -ge "$2" ]]; then
+            echo "$path"
+        fi
+    done
+else
+    find "$1" -type l -printf '%p\n' | while read -r path; do
+        if [[ ! -e "$path" ]]; then
+            echo "$path"
+        fi
+    done
+fi
+########################################################
+
+
+#!/bin/bash
+
+if [[ $# -lt 1 || $# -gt 2 ]]; then
     echo "Usage: $0 DIR [N]" >&2
     exit 1
 fi
