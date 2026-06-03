3a732112a23f4321c920d221aebf35114a75672e
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-06-03 23:29:45 +0300

    Update Task 11 2017-SE-02.sh

--- 02.Scripts (topics 1-5)/Task 11 2017-SE-02.sh
+++ 02.Scripts (topics 1-5)/Task 11 2017-SE-02.sh
@@ -4,43 +4,30 @@ if [[ $# -ne 3 ]]; then
     exit 1
 fi
 
-src="${1}"
-dst="${2}"
-abc="${3}"
-#must be root
-if [[ "$(id -u)" -ne 0 ]]; then
- echo "Error: must be run as root" >&2
-  exit 1
-fi
-#validate dirs
+src="$1"
+dst="$2"
+str="$3"
 
-if [[ ! -d "$src" ]]; then
- echo "Error: SRC is not a directory" >&2
-  exit 1
+if [[ "$(whoami)" != "root" ]]; then
+    exit 1
 fi
 
-if [[ ! -d "$dst" ]]; then
-  echo "Error: DST is not a directory or dst is not empty" >&2
-  exit 1
+if [[ ! -d "$src" || ! -d "$dst" ]]; then
+    exit 1
 fi
 
-if [[ -z "$(find "$src" -mindepth 1)" ]]; then 
-  echo "The first directory must not be empty..." >&2
-  exit 2
-fi
+find "$src" -type f | while read -r file; do
 
-if [[ -n "$(find "$dst" -mindepth 1)" ]]; then 
-  echo "The 2nd directory must  be empty..." >&2
-  exit 2
-fi
+    if echo "$file" | grep -q "$str"; then
 
-# find files whose *name* contains abc, move preserving structure
+        relpath=$(echo "$file" | sed "s#^$src/##")
 
-find "$src" -type f -name "*$abc*" -print0 | 
-while IFS= read -r -d '' file;do       #d stands for stop reading at null character
-  #to remove the src prefix
-  relPath=$(echo "$file" | sed "s|^$src/||")
-  mkdir -p "$dst/$(dirname "$relpath")"
-  mv "$file" "$dst/$relpath"
+        dir=$(dirname "$relpath")
+
+        mkdir -p "$dst/$dir"
+
+        mv "$file" "$dst/$relpath"
+
+    fi
+
 done
-
