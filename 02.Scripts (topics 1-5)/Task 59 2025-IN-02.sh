636a6862d9ea43ead5526bebc86234a864949fad
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-06-08 22:56:18 +0300

    Update Task 59 2025-IN-02.sh

--- 02.Scripts (topics 1-5)/Task 59 2025-IN-02.sh
+++ 02.Scripts (topics 1-5)/Task 59 2025-IN-02.sh
@@ -1,4 +1,44 @@
 
+
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    exit 1
+fi
+
+domain="$1"
+file="$2"
+
+if [[ ! -f "$file" ]]; then
+    exit 2
+fi
+
+teams=$(mktemp)
+
+cat "$file" | tr -s ' ' | cut -d ' ' -f3 | sort | uniq > "$teams"
+
+while read -r artist; do
+    echo "; team $artist"
+
+    zones=$(grep -E " $artist$" "$file" | tr -s ' ' | cut -d ' ' -f2 | sort | uniq)
+    servers=$(grep -E " $artist$" "$file" | tr -s ' ' | cut -d ' ' -f1 | sort | uniq)
+
+    for z in $zones; do
+        for s in $servers; do
+            echo "$z IN NS $s.$domain."
+        done
+    done
+
+done < "$teams"
+
+rm -f "$teams"
+
+##########################################################################################################
+
+
+
+
+
 #!/bin/bash
 
 if [[ $# -ne 2 ]]; then
