744ee5cf214dfa09a6918c134a90bd32ce5b2608
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-06-18 16:23:14 +0300

    Update Task 19 !! 2025-SE-02.c

--- 04.C (processes, pipes, input&output)/Task 19 !! 2025-SE-02.c
+++ 04.C (processes, pipes, input&output)/Task 19 !! 2025-SE-02.c
@@ -1,5 +1,215 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdint.h>
+#include <err.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define MAXFILE 16
+#define PACKET 64
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3 || argc > MAXFILE + 2) {
+        errx(1, "usage: %s producer file1 [file2 ...]", argv[0]);
+    }
+
+    int n = argc - 2;
+
+    int files[MAXFILE];
+
+    for (int i = 0; i < n; i++) {
+        files[i] = open(argv[i + 2], O_WRONLY | O_APPEND);
+        if (files[i] < 0) {
+            err(2, "open consumer");
+        }
+    }
+
+    int pipefd[2];
+
+    if (pipe(pipefd) < 0) {
+        err(3, "pipe");
+    }
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        err(4, "fork");
+    }
+
+    if (pid == 0) {
+        close(pipefd[0]);
+
+        if (dup2(pipefd[1], 1) < 0) {
+            err(5, "dup2");
+        }
+
+        close(pipefd[1]);
+
+        execl(argv[1], argv[1], (char*)NULL);
+        err(6, "exec");
+    }
+
+    close(pipefd[1]);
+
+    uint8_t packet[PACKET];
+    int index = 0;
+
+    while (1) {
+        ssize_t bytesRead = read(pipefd[0], packet, sizeof(packet));
+
+        if (bytesRead == 0) {
+            break;
+        }
+
+        if (bytesRead < 0) {
+            err(7, "read");
+        }
+
+        if (bytesRead != sizeof(packet)) {
+            errx(8, "incomplete packet");
+        }
+
+        if (write(files[index], packet, sizeof(packet)) != sizeof(packet)) {
+            err(9, "write");
+        }
+
+        index++;
+
+        if (index == n) {
+            index = 0;
+        }
+    }
+
+    close(pipefd[0]);
+
+    for (int i = 0; i < n; i++) {
+        close(files[i]);
+    }
+
+    int status;
+    if (waitpid(pid, &status, 0) < 0) {
+        err(10, "waitpid");
+    }
+
+    if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+        errx(11, "producer failed");
+    }
+
+    exit(0);
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <err.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define MAXFILE 16
+#define PACKET 64
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3 || argc > MAXFILE + 2) {
+        errx(1, "wrong arguments");
+    }
+
+    int n = argc - 2;
+
+    char* files[MAXFILE];
+
+    for (int i = 0; i < n; i++) {
+        files[i] = argv[i + 2];
+    }
+
+    int client[2];
+
+    if (pipe(client) < 0) {
+        err(1, "pipe");
+    }
+
+    pid_t clientPID = fork();
+
+    if (clientPID < 0) {
+        err(1, "fork");
+    }
+
+    if (clientPID == 0) {
+        close(client[0]);
+
+        if (dup2(client[1], 1) < 0) {
+            err(1, "dup2");
+        }
+
+        close(client[1]);
+
+        execl(argv[1], argv[1], (char*)NULL);
+        err(1, "execl");
+    }
+
+    close(client[1]);
+
+    uint8_t packet[PACKET];
+    int index = 0;
+
+    while (1) {
+        ssize_t bytesRead = read(client[0], packet, sizeof(packet));
+
+        if (bytesRead == 0) {
+            break;
+        }
+
+        if (bytesRead < 0) {
+            err(1, "read");
+        }
+
+        if (bytesRead != sizeof(packet)) {
+            errx(1, "incomplete packet");
+        }
+
+        pid_t pid = fork();
+
+        if (pid < 0) {
+            err(1, "fork");
+        }
+
+        if (pid == 0) {
+            int fd = open(files[index], O_WRONLY | O_APPEND);
+
+            if (fd < 0) {
+                err(1, "open");
+            }
+
+            if (write(fd, packet, sizeof(packet)) != sizeof(packet)) {
+                err(1, "write");
+            }
+
+            close(fd);
+            exit(0);
+        }
+
+        waitpid(pid, NULL, 0);
+
+        index++;
+
+        if (index == n) {
+            index = 0;
+        }
+    }
+
+    close(client[0]);
+
+    waitpid(clientPID, NULL, 0);
+
+    exit(0);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <err.h>
 #include <sys/wait.h>
