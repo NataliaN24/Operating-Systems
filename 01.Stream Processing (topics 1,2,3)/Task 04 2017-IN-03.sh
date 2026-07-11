fa1101f3add5380433772f71ead17721f735ef28
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-07-11 16:02:35 +0300

    Update Task 04 2017-IN-03.sh

--- 01.Stream Processing (topics 1,2,3)/Task 04 2017-IN-03.sh
+++ 01.Stream Processing (topics 1,2,3)/Task 04 2017-IN-03.sh
@@ -1,3 +1,15 @@
+grep "Inf" /etc/passwd \
+| cut -d ':' -f1,5 \
+| cut -d ',' -f1 \
+| grep -E '^.+ .+a$' \
+| cut -d ':' -f1 \
+| cut -c3-4 \
+| sort \
+| uniq -c \
+| sort -nr \
+| head -n1
+
+######################################################################################
 cut -d: -f1,5 /etc/passwd |
 grep "Inf" |
 grep -E "[A-Za-z]+a," |
