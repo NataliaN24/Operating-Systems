59e2acad46de712149b44db28ab6aa9a1d4d1d12
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-07-11 18:20:30 +0300

    Update Task 16  2023-CE-02.sh

--- 01.Stream Processing (topics 1,2,3)/Task 16  2023-CE-02.sh
+++ 01.Stream Processing (topics 1,2,3)/Task 16  2023-CE-02.sh
@@ -1,1 +1,4 @@
+find /var/log/my_logs -type f | grep -E '/[[:alnum:]_]+_[0-9]+\.log$' | xargs grep -o -w "error" | wc -l
+
+######################################################################################
 find /var/log/my_logs -type f -name "*.log" 2>/dev/null | grep -E '/[A-Za-z0-9_]+_[0-9]+\.log$' | xargs grep -o 'error' 2>/dev/null | wc -l
