4d4f8bc24875bfacde8f2d3397c0dfcde6469d8a
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-07-12 17:02:23 +0300

    Update Task 09  2017-IN-03 .sh

--- 02.Scripts (topics 1-5)/Task 09  2017-IN-03 .sh
+++ 02.Scripts (topics 1-5)/Task 09  2017-IN-03 .sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+tmp=$(mktemp)
+
+while IFS=: read -r user x uid gid comment home shell; do
+
+    if [[ ! -d "$home" ]]; then
+        continue
+    fi
+
+    lastModifiedFile=$(find "$home" -type f -printf "%T@ %p\n" | sort -nr | head -n 1)
+
+    if [[ -n "$lastModifiedFile" ]]; then
+        echo "$user:$lastModifiedFile" >> "$tmp"
+    fi
+
+done < /etc/passwd
+
+
+sort -t ':' -k2,2nr "$tmp" | head -n 1
+
+rm "$tmp"
+###############################################33
+
+#!/bin/bash
+
 if [[ $# -ne 0 ]]; then
   echo "Usage: $0" >&2
   exit 1
