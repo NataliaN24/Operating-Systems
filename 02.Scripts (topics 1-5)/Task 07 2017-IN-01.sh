44c6640915415e816edc09ff755d3fe6a8d4e8c2
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-07-12 16:46:41 +0300

    Update Task 07 2017-IN-01.sh

--- 02.Scripts (topics 1-5)/Task 07 2017-IN-01.sh
+++ 02.Scripts (topics 1-5)/Task 07 2017-IN-01.sh
@@ -1,5 +1,67 @@
 #!/bin/bash
 
+if [[ $# -ne 3 ]]; then
+    exit 1
+fi
+
+file="$1"
+str1="$2"
+str2="$3"
+
+if [[ ! -f "$file" ]]; then
+    exit 2
+fi
+
+tmp=$(mktemp)
+
+# Намираме стойността на str1
+terms1=""
+
+while read -r line; do
+    if echo "$line" | grep -q "^$str1="; then
+        terms1=$(echo "$line" | cut -d '=' -f2-)
+        break
+    fi
+done < "$file"
+
+# Обработваме файла
+while read -r line; do
+
+    if ! echo "$line" | grep -q "^$str2="; then
+        echo "$line" >> "$tmp"
+        continue
+    fi
+
+    terms2=$(echo "$line" | cut -d '=' -f2-)
+
+    finalTerms=""
+
+    for opt in $terms2; do
+        found=0
+
+        for opt2 in $terms1; do
+            if [[ "$opt" == "$opt2" ]]; then
+                found=1
+                break
+            fi
+        done
+
+        if [[ "$found" -eq 0 ]]; then
+            finalTerms="$finalTerms $opt"
+        fi
+    done
+
+    finalTerms=$(echo "$finalTerms" | xargs)
+
+    echo "$str2=$finalTerms" >> "$tmp"
+
+done < "$file"
+
+mv "$tmp" "$file"
+########################################################
+
+#!/bin/bash
+
 if [[ $# -ne 3 ]];then
   echo "three args"
   exit 1;
