fbf4f4deb79cf2246666d0584dc8d8748baec820
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-07-13 22:38:54 +0300

    Update Task 17 2018-SE-02.sh

--- 02.Scripts (topics 1-5)/Task 17 2018-SE-02.sh
+++ 02.Scripts (topics 1-5)/Task 17 2018-SE-02.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+number=0
+file="$1/dict.txt"
+
+touch "$file"
+
+while read -r line; do
+
+    name=$(echo "$line" | cut -d ' ' -f1)
+    last=$(echo "$line" | cut -d ' ' -f2)
+
+    reply=$(echo "$line" | sed 's/([^)]*)//' | tr -s ' ' | cut -d ':' -f2-)
+
+
+    if grep -Eq "^$name $last;" "$file"; then
+
+        num=$(grep -E "^$name $last;" "$file" | cut -d ';' -f2)
+
+        echo "$reply" >> "$1/$num.txt"
+
+    else
+
+        number=$((number+1))
+
+        echo "$name $last;$number" >> "$file"
+
+        touch "$1/$number.txt"
+
+        echo "$reply" >> "$1/$number.txt"
+
+    fi
+
+done < "$2"
+###########################################################
+
+#!/bin/bash
+
 if [[ $# -ne 2 ]]; then
   echo "Usage: $0 <file> <directory> " >&2
   exit 1
