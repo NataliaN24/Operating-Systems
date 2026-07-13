6023e14994a49b30da574dd56dcf3837ffa2b346
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-07-13 22:19:41 +0300

    Update Task 16  2018-SE-01.sh

--- 02.Scripts (topics 1-5)/Task 16  2018-SE-01.sh
+++ 02.Scripts (topics 1-5)/Task 16  2018-SE-01.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 
 if [[ $# -ne 1 ]]; then
+    exit 1
+fi
+
+records=$(mktemp)
+
+# Събираме приятел и брой редове за всеки лог
+while read -r file; do
+    friend=$(basename "$(dirname "$file")")
+    lines=$(wc -l < "$file")
+
+    echo "$friend $lines" >> "$records"
+
+done < <(find "$1" -type f | grep -E '[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{4}\.txt$')
+
+
+final=$(mktemp)
+
+# Сумираме всички разговори с един и същ приятел
+while read -r friend count; do
+
+    current=$(grep "^$friend " "$final" | cut -d' ' -f2)
+
+    if [[ -z "$current" ]]; then
+        echo "$friend $count" >> "$final"
+    else
+        new=$((current + count))
+        sed -i "s/^$friend .*/$friend $new/" "$final"
+    fi
+
+done < "$records"
+
+
+sort -k2 -nr "$final" | head -n 10
+
+#################################################3
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
     echo "Usage: $0 LOGDIR"
     exit 1
 fi
