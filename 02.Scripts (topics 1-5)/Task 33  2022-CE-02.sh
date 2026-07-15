29fc58b9ecce57280be411bfc2ce225d403f29d7
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-07-16 00:11:44 +0300

    Update Task 33  2022-CE-02.sh

--- 02.Scripts (topics 1-5)/Task 33  2022-CE-02.sh
+++ 02.Scripts (topics 1-5)/Task 33  2022-CE-02.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-if [[ $# -ne 1]];then
-  exit 1
+if [[ $# -ne 1 ]]; then
+    exit 1
 fi
 
-device=${1}
+device="$1"
 file="/proc/acpi/wakeup"
 
-if [[ ! "$device" =~ ^[A-Z0-9]{1,4}$ ]];then
-  exit 1
+if [[ ! "$device" =~ ^[A-Z0-9]{1,4}$ ]]; then
+    exit 1
 fi
 
 row=$(grep "^$device[[:space:]]" "$file")
 
-if [[ -z "$row"]];then
-  exit 2;
+if [[ -z "$row" ]]; then
+    exit 2
 fi
 
-status=$(echo "$row" | awk '{print $3}')
-if [[ "$status" == "*disabled" ]];then
-  exit 0
+status=$(echo "$row" | tr -s '[:blank:]' | cut -d ' ' -f3)
+
+if [[ "$status" == "*disabled" ]]; then
+    exit 0
 fi
-echo $device" > "$file"
+
+echo "$device" > "$file"
