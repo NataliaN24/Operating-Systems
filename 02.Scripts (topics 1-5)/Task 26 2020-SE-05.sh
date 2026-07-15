d335951486af7521cdf51d1ff0c3afc11043bcd9
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-07-15 18:41:30 +0300

    Update Task 26 2020-SE-05.sh

--- 02.Scripts (topics 1-5)/Task 26 2020-SE-05.sh
+++ 02.Scripts (topics 1-5)/Task 26 2020-SE-05.sh
@@ -1,5 +1,76 @@
 #!/bin/bash
 
+pwd="$1"
+config="$2"
+dir="$3"
+
+# временен файл за валидните cfg файлове
+validFile=$(mktemp)
+
+find "$dir" -type f -name "*.cfg" -print0 | while IFS= read -r -d '' file
+do
+    errors=$(mktemp)
+
+    count=1
+    found=0
+
+    while IFS= read -r line
+    do
+        # коментар
+        if [[ "$line" =~ ^# ]]; then
+            count=$((count+1))
+            continue
+        fi
+
+        # валиден ред
+        if ! echo "$line" | grep -Eq '^\{ [a-zA-Z-]+ \};$'
+        then
+            found=1
+            echo "Line $count:$line" >> "$errors"
+        fi
+
+        count=$((count+1))
+
+    done < "$file"
+
+
+    # ако има грешки
+    if [[ "$found" -eq 1 ]]
+    then
+        echo "Error in $file:"
+        cat "$errors"
+
+    else
+        # добавяме към общия config
+        cat "$file" >> "$validFile"
+
+
+        # име без .cfg = потребител
+        username=$(basename "$file" .cfg)
+
+
+        # проверка дали го има в pwd файла
+        if ! grep -q "^$username:" "$pwd"
+        then
+            # генериране на парола
+            password=$(pwgen 16 1)
+
+            # md5 на паролата
+            hash=$(echo -n "$password" | md5sum | cut -d' ' -f1)
+
+            echo "$username:$hash" >> "$pwd"
+
+            echo "$username $password"
+        fi
+    fi
+
+done
+
+# запис на крайния config
+mv "$validFile" "$config"
+//////////////////////////////////////////////////////////////////////////////
+#!/bin/bash
+
 if [[ $# -ne 3 ]]; then
     echo "Usage: $0 foo.pwd config.cfg cfgdir"
     exit 1
