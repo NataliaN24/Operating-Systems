9f6b6bbd4a21e679b0b34ab7f71f7586c9fcfc7b
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-07-19 16:13:06 +0300

    Update Task 53  2024-SE-02.sh

--- 02.Scripts (topics 1-5)/Task 53  2024-SE-02.sh
+++ 02.Scripts (topics 1-5)/Task 53  2024-SE-02.sh
@@ -1,5 +1,68 @@
 #!/bin/bash
 
+localFile="/etc/passwd"
+
+if [[ -n "$PASSWD" ]]; then
+    localFile="$PASSWD"
+fi
+
+# директорията, в която е скриптът (за да намерим occ)
+scriptDir="$(dirname "$0")"
+
+# локални потребители (UID >= 1000)
+users=$(mktemp)
+
+awk -F: '$3 >= 1000 {print $1}' "$localFile" > "$users"
+
+
+# PrevCloud потребители
+userInCloud=$(mktemp)
+
+"$scriptDir/occ" user:list > "$userInCloud"
+
+
+# Синхронизация локални -> PrevCloud
+while read -r user; do
+
+    if ! grep -Eq "^- $user:" "$userInCloud"; then
+        # няма такъв потребител в PrevCloud
+        "$scriptDir/occ" user:add "$user"
+
+    else
+        # има потребител, проверяваме дали е заключен
+        enabled=$("$scriptDir/occ" user:info "$user" | grep "enabled:" | cut -d ':' -f2 | tr -d ' ')
+
+        if [[ "$enabled" == "false" ]]; then
+            "$scriptDir/occ" user:enable "$user"
+        fi
+    fi
+
+done < "$users"
+
+
+
+# Синхронизация PrevCloud -> локални
+while read -r line; do
+
+    user=$(echo "$line" | cut -d ':' -f1 | sed 's/ - //')
+
+    if ! grep -Eq "^$user$" "$users"; then
+
+        enabled=$("$scriptDir/occ" user:info "$user" | grep "enabled:" | cut -d ':' -f2 | tr -d ' ')
+
+        if [[ "$enabled" == "true" ]]; then
+            "$scriptDir/occ" user:disable "$user"
+        fi
+
+    fi
+
+done < "$userInCloud"
+
+
+rm "$users" "$userInCloud"
+-------------------------------------------------------------------------------------------------
+#!/bin/bash
+
 if [[ -z "$PASSWD" ]]; then
     passwd_file="/etc/passwd"
 else
