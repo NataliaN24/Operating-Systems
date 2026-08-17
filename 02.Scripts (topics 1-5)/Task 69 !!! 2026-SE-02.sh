7279289a498e28d86ff62358cf9eb310a1f67358
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-08-17 22:05:54 +0300

    Update Task 69 !!! 2026-SE-02.sh

--- 02.Scripts (topics 1-5)/Task 69 !!! 2026-SE-02.sh
+++ 02.Scripts (topics 1-5)/Task 69 !!! 2026-SE-02.sh
@@ -12,6 +12,70 @@ fi
 
 cmd=""
 workdir=""
+
+while read -r line; do
+
+    # Премахваме коментара
+    line=$(echo "$line" | sed 's/#.*//')
+
+    if [[ -z "$line" ]]; then
+        continue
+    fi
+
+    # CMD
+    if [[ "$line" == CMD* ]]; then
+        cmd=$(echo "$line" | cut -d' ' -f2-)
+
+    # ARGS
+    elif [[ "$line" == ARGS* ]]; then
+        args=$(echo "$line" | sed 's/^ARGS //' | sed 's/^\[//' | sed 's/\]$//' | sed 's/", "/\n/g' | tr -d '"')
+
+    # ENV
+    elif [[ "$line" == ENV* ]]; then
+        envs=$(echo "$line" | sed 's/^ENV //' | sed 's/^{//' | sed 's/}$//' | sed 's/", "/\n/g' | tr -d '"')
+
+    # WORKDIR
+    elif [[ "$line" == WORKDIR* ]]; then
+        workdir=$(echo "$line" | cut -d' ' -f2-)
+    fi
+
+done < "$file"
+
+if [[ -n "$workdir" ]]; then
+    cd "$workdir" || exit 3
+fi
+
+echo "$args" > args.tmp
+echo "$envs" > envs.tmp
+
+command="$cmd"
+
+while read -r arg; do
+    command="$command \"$arg\""
+done < args.tmp
+
+while read -r env; do
+    command="$env $command"
+done < envs.tmp
+
+eval "$command"
+
+rm args.tmp envs.tmp
+////////////////////////////////////////////////////////////////////
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    exit 1
+fi
+
+file="$1"
+
+if [[ ! -f "$file" ]]; then
+    exit 2
+fi
+
+cmd=""
+workdir=""
 args=()
 envs=()
 
