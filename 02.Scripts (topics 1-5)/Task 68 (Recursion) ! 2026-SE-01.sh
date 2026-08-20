fb3784e574ab187a8120f5b9bd6b6f9d2912fc98
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2026-08-20 14:28:23 +0300

    Update and rename Task 68 (Recursion) !!! 2026-SE-01.sh to Task 68 (Recursion) ! 2026-SE-01.sh

--- 02.Scripts (topics 1-5)/Task 68 (Recursion) ! 2026-SE-01.sh
+++ 02.Scripts (topics 1-5)/Task 68 (Recursion) ! 2026-SE-01.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
+file="$1"
+
+if [[ ! -f "$file" ]]; then
+    exit 1
+fi
+
+include_func()
+{
+    local f="$1"
+    local dir
+    local line
+    local newFile
+    local path
+
+    dir=$(dirname "$f")
+
+    while read -r line; do
+        if ! echo "$line" | grep -q "^!include:.+"; then
+            echo "$line"
+        else
+            newFile=$(echo "$line" | cut -d ':' -f2-)
+
+            path="$dir/$newFile"
+
+            if [[ ! -f "$path" ]]; then
+                exit 2
+            fi
+
+            include_func "$path"
+        fi
+    done < "$f"
+}
+
+include_func "$file"
+/////////////////////////////
+#!/bin/bash
+
 if [[ $# -ne 1 ]]; then
     exit 1
 fi
